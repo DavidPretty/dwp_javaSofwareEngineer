885d77dbac38d83855833faa4bc97dfe2bef2d96
David Pretty <davidpretty2001@yahoo.co.uk>
2020-06-14 12:48:47 +0000

    remove proxy data as not needed

--- flask_app.py
+++ flask_app.py
@@ -4,15 +4,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-proxies = {
-  "http": "proxy.server:3128",
-  "https": "proxy.server:3128",
-} #specific to Pythonanywhere
 
 @app.route('/')
 def index():
     return jsonify(message =  "hello world")
 
 def get_londoners():
-    return requests.get("https://bpdts-test-app.herokuapp.com/city/London/users",
-    proxies)
+    return requests.get("https://bpdts-test-app.herokuapp.com/city/London/users")
