726ea37e19e6756a538b896b8f1c6e4ed2951380
David Pretty <davidpretty2001@yahoo.co.uk>
2020-06-14 15:27:51 +0000

    add stub get_haversine method

--- flask_app.py
+++ flask_app.py
@@ -12,5 +12,5 @@ def index():
 def get_londoners():
     return requests.get("https://bpdts-test-app.herokuapp.com/city/London/users")
 
-def get_haversine():
-    pass
+def get_haversine(latX, lonX, latY, lonY):
+    return 0
