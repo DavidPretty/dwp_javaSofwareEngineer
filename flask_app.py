d0e473cdfe95f095f9617649cc2e9e490ed14e8b
David Pretty <davidpretty2001@yahoo.co.uk>
2020-06-14 11:57:12 +0000

    add simple test to see if can call heroku app to get londonders

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,18 @@
-import requests
+import requests, urllib3
 
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+proxies = {
+  "http": "proxy.server:3128",
+  "https": "proxy.server:3128",
+} #specific to Pythonanywhere
 
 @app.route('/')
 def index():
     return jsonify(message =  "hello world")
 
-def getLondoners():
-    pass
+def get_londoners():
+    return requests.get("https://bpdts-test-app.herokuapp.com/city/London/users",
+    proxies)
--- test_flask_app.py
+++ test_flask_app.py
@@ -1,8 +1,8 @@
 import pytest
 
-import flask_app
+import flask_app as fa
 
-app = flask_app.app
+app = fa.app
 
 @pytest.fixture
 def client():
@@ -12,3 +12,7 @@ def test_get_dummy_json():
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert response.is_json
+
+def test_get_londoners_not_return_none():
+    londoners = fa.get_londoners()
+    assert not londoners is None
