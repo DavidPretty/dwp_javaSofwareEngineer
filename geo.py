a910cbff5ede84ec63e32f29a85a30d9bc42ae40
David Pretty <davidpretty2001@yahoo.co.uk>
2020-09-28 16:52:00 +0000

    encapsulate constants in geo.py into methods

--- geo.py
+++ geo.py
@@ -1,10 +1,8 @@
 from math import radians, sin, cos, sqrt, asin
 
-LONDON_LAT = 51.50868648029151 #Trafalgar Square
-LONDON_LONG = -0.1276495 #Trafalgar Square
-R = 3958.8  # Earth's radius in miles
 
 def get_haversine(latX, lonX, latY, lonY):
+    R = 3958.8  # Earth's radius in miles
 
     diffLat = radians(latY - latX)
     diffLon = radians(lonY - lonX)
@@ -17,4 +15,7 @@ def get_haversine(latX, lonX, latY, lonY):
     return R * c
 
 def is_within_50_miles_of_London(latX, lonX):
+    LONDON_LAT = 51.50868648029151 #Trafalgar Square
+    LONDON_LONG = -0.1276495 #Trafalgar Square
+
     return get_haversine(latX, lonX, LONDON_LAT, LONDON_LONG) <= 50
