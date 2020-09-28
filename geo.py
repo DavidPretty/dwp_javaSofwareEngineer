0daa67181ef307be6eb56387a081bc872bcd09b9
David Pretty <davidpretty2001@yahoo.co.uk>
2020-09-28 16:43:46 +0000

    remove NEARBY constant from geo.py as encapsulating in is_within_50_miles_of_London() method

--- geo.py
+++ geo.py
@@ -2,7 +2,6 @@ from math import radians, sin, cos, sqrt, asin
 
 LONDON_LAT = 51.50868648029151 #Trafalgar Square
 LONDON_LONG = -0.1276495 #Trafalgar Square
-NEARBY = 50
 R = 3958.8  # Earth's radius in miles
 
 def get_haversine(latX, lonX, latY, lonY):
@@ -18,4 +17,4 @@ def get_haversine(latX, lonX, latY, lonY):
     return R * c
 
 def is_within_50_miles_of_London(latX, lonX):
-    return get_haversine(latX, lonX, LONDON_LAT, LONDON_LONG) <= NEARBY
+    return get_haversine(latX, lonX, LONDON_LAT, LONDON_LONG) <= 50
