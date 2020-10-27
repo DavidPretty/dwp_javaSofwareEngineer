84d7e132b4a912a39c9de82a6c0adbbb0d10cdad
David Pretty <davidpretty2001@yahoo.co.uk>
2020-10-27 18:06:53 +0000

    test and app files deleted from root directory, have been moved to test & flaskr directories respsectively in earlier commit

--- user_data.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def get_londoners():
-    return requests.get("https://bpdts-test-app.herokuapp.com/city/London/users")
-
-def get_users():
-    return requests.get("https://bpdts-test-app.herokuapp.com/users")
