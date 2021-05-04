33eff9701977f53cb278f07d7a7545c8935157b7
David Pretty <davidpretty2001@yahoo.co.uk>
2021-05-04 20:27:24 +0000

    amend name of called method so matches method definition

--- tests/test_user_data.py
+++ tests/test_user_data.py
@@ -22,7 +22,7 @@ def get_mock_londoners():
 
 @patch("mysite.flaskr.user_data.requests.get")
 def test_get_londoners_returns_status_ok(get_mock_londoners):
-    mock_get_londoners.return_value.ok = True
+    get_mock_londoners.return_value.ok = True
     londoners = user_data.get_londoners()
     assert(londoners.ok)
 
